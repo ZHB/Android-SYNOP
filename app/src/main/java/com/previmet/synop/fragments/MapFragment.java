a1c46d2dd9607ab5b6aa93086089366cc7a0b508
Vincent <vincent.huck.pro@gmail.com>
2014-12-21 22:19:54 +0100

    Google Maps settings

--- app/src/main/java/com/previmet/synop/fragments/MapFragment.java
+++ app/src/main/java/com/previmet/synop/fragments/MapFragment.java
@@ -2,6 +2,7 @@ package com.previmet.synop.fragments;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.renderscript.Script;
 import android.support.v4.app.Fragment;
@@ -17,11 +18,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.previmet.synop.R;
@@ -38,16 +43,17 @@ import java.util.ArrayList;
 public class MapFragment extends SupportMapFragment   {
 
 
-    GoogleMap mapView;
+    GoogleMap googleMap;
 
     @Override
     public void onCreate(Bundle arg0) {
+
         super.onCreate(arg0);
     }
 
     @Override
-    public View onCreateView(LayoutInflater mInflater, ViewGroup arg1,
-                             Bundle arg2) {
+    public View onCreateView(LayoutInflater mInflater, ViewGroup arg1, Bundle arg2) {
+
         return super.onCreateView(mInflater, arg1, arg2);
     }
 
@@ -60,11 +66,72 @@ public class MapFragment extends SupportMapFragment   {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mapView = getMap();
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.draggable(true);
-        markerOptions.position(new LatLng(46.4594, 8.9356));
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker());
-        mapView.addMarker(markerOptions);
+
+        // Getting Google Play availability status
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+
+        // display a connectivity error message if needed
+        if(status != ConnectionResult.SUCCESS){
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, getActivity(), 10);
+            dialog.show();
+        } else {
+
+            createMapView();
+            addMarker();
+
+            // Zoom the map
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(46.4594, 4.7356)).zoom(5.0f).build();
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+            googleMap.moveCamera(cameraUpdate);
+        }
+    }
+
+    /**
+     * Initialises the mapview
+     */
+    private void createMapView(){
+        /**
+         * Catch the null pointer exception that
+         * may be thrown when initialising the map
+         */
+        try {
+            if(null == googleMap){
+                googleMap = getMap();
+
+                /**
+                 * If the map is still null after attempted initialisation,
+                 * show an error to the user
+                 */
+                if(null == googleMap) {
+                    Toast.makeText(getActivity().getApplicationContext(),"Error creating map",Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+
+    /**
+     * Adds a marker to the map
+     */
+    private void addMarker(){
+
+        DbCursor sCursor = Db.getStations();
+
+        /** Make sure that the map has been initialised **/
+        if(null != googleMap){
+
+            while(sCursor.moveToNext()) {
+                // The Cursor is now set to the right position
+
+                googleMap.addMarker(new MarkerOptions()
+                                .position(
+                                        new LatLng(sCursor.getDouble(sCursor.getColumnIndex(DbContract.Station.COLUMN_NAME_LATITUDE)),
+                                                   sCursor.getDouble(sCursor.getColumnIndex(DbContract.Station.COLUMN_NAME_LONGITUDE))))
+                                .title(sCursor.getString(sCursor.getColumnIndex(DbContract.Station.COLUMN_NAME_STATION)))
+                                .draggable(false)
+                );
+            }
+        }
     }
 }
